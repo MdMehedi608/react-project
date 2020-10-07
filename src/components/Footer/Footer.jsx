b174449ca6748e6bbbb3a3008627aabc212544ba
NazmulHimel <himu1110@gmail.com>
2020-10-08 01:39:56 +0600

    update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer>
+        <div className={['container', {classes.container}]} id="footer">
+          <div>
+            Copyright &copy;2020.
+          </div>
+          <div className="textRight">
+            Built with <span className="red">&hearts;</span> by
+            <a
+              href="https://reactjs.org/docs/create-a-new-react-app.html"
+              target="_blank"
+              >Create React App</a
+            >
+          </div>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+import reactLogo from '../../assets/images/react.svg';
+import classes from './Header.module.css';
+
+function Header() {
+    return (
+        <header>
+        <div className="container">
+          <div>
+            <img className={classes.logo} src={logo} alt="Logo" />
+          </div>
+          <div className="textRight">
+            <img src={reactLogo} alt="React" className={reactLogo} /><strong
+              >React</strong
+            >
+          </div>
+        </div>
+      </header>
+    )
+}
+
+export default Header
